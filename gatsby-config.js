88bad7ae0519fe5ae3ac0d27b8b646966997fe75
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-05-26 13:52:09 +0200

    emotion setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
 });
 
 const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+	spaceId: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 };
 
 // if you want to use the preview API please define
@@ -12,30 +12,31 @@ const contentfulConfig = {
 // the `host` property should map to `preview.contentful.com`
 // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
 if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+	contentfulConfig.host = process.env.CONTENTFUL_HOST;
 }
 
 const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
-  throw new Error(
-    "Contentful spaceId and the access token need to be provided."
-  );
+	throw new Error(
+		'Contentful spaceId and the access token need to be provided.'
+	);
 }
 
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Contentful starter",
-  },
-  pathPrefix: "/gatsby-contentful-starter",
-  plugins: [
-    "gatsby-transformer-remark",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-source-contentful",
-      options: contentfulConfig,
-    },
-  ],
+	siteMetadata: {
+		title: 'Gatsby Contentful starter',
+	},
+	pathPrefix: '/gatsby-contentful-starter',
+	plugins: [
+		'gatsby-plugin-emotion',
+		'gatsby-transformer-remark',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-contentful',
+			options: contentfulConfig,
+		},
+	],
 };
