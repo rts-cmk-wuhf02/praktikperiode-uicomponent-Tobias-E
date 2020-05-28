f7c96cdd2e1a2854c958d3085558fb7913ac0999
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-05-28 14:41:32 +0200

    created file Component2

---
+++ src/components/component2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import { css } from '@emotion/core';
+
+const Component2 = () => {
+	return;
+};
+
+export default Component2;
